242495dcb68b7b50fbac791bcba2337a4a89a683
erickmt <erickmoreiratc@gmail.com>
2022-12-14 13:21:29 -0300

    deploy

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
+//Install express server
 const express = require('express');
+const path = require('path');
 
-const path = require('path');   
+const app = express();
 
-const app = express();   
+app.use(express.static(__dirname + '/dist/gcbc-puc'));
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/gcbc-puc/index.html'));
+});
 
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
-});  
-
-// Start the app by listening on the default Heroku port    
 app.listen(process.env.PORT || 8080);
